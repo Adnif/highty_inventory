c6920b9f5d018a6b63b839336c8bc6616c7b7c0c
Adnif <ahmadbani9@gmail.com>
2024-06-18 10:29:58 +0700

    final fe

--- lib/presentation/screens/qr_check.dart
+++ lib/presentation/screens/qr_check.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:highty_inventory/presentation/constants/fonts.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class QrCheck extends StatefulWidget {
   final String receipt;
@@ -11,6 +15,9 @@ class QrCheck extends StatefulWidget {
 }
 
 class _QrCheckState extends State<QrCheck> {
+  final String sama = '#Q543234';
+  final String beda = '#Q543434';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +27,36 @@ class _QrCheckState extends State<QrCheck> {
           style: primary,
         ),
       ),
+      body: MobileScanner(
+        controller: MobileScannerController(
+          detectionSpeed: DetectionSpeed.noDuplicates,
+          returnImage: true,
+        ),
+        onDetect:  (capture){
+          final List<Barcode> barcodes = capture.barcodes;
+          final Uint8List? image = capture.image;
+          for(final barcode in barcodes) {
+            log('Barcode found! ${barcode.rawValue}');
+          }
+          if(image != null){
+            // showDialog(
+            //   context: context,
+            //   builder: (context){
+            //     return AlertDialog(
+            //       title: Text(
+            //         barcodes.first.rawValue ?? "",
+            //       ),
+            //       content: Image(
+            //         image: MemoryImage(image),
+            //       ),
+            //     );
+            //   }
+            // );
+            
+          }
+          
+        },
+      )
     );
   }
 }
